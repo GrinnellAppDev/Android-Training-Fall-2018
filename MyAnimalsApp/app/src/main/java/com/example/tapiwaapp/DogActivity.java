8144caa0f26975fe2c0975ac8e449e842ad3949f
Tapszvidzwa <zvidzwat@grinnell.edu>
2018-11-01 18:56:55 -0500

    Added AsyncTask

--- MyAnimalsApp/app/src/main/java/com/example/tapiwaapp/DogActivity.java
+++ MyAnimalsApp/app/src/main/java/com/example/tapiwaapp/DogActivity.java
@@ -1,9 +1,12 @@
 package com.example.tapiwaapp;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,7 +15,7 @@ import com.squareup.picasso.Picasso;
 public class DogActivity extends AppCompatActivity {
 
     ImageView imageView;
-    TextView pageVisitsCount;
+    TextView pageVisitsCount, dogslept;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,7 @@ public class DogActivity extends AppCompatActivity {
 
         imageView = findViewById(R.id.dogView);
         pageVisitsCount = findViewById(R.id.dogPageVisitsCount);
+        dogslept = findViewById(R.id.dogSleepingTime);
 
         loadDogImage();
         incrementPageVisit();
@@ -56,7 +60,61 @@ public class DogActivity extends AppCompatActivity {
         pageVisitsCount.setText("Page visited " + currentPageVisits + " times");
     }
 
+    public void runSleep(View view) {
+        AsyncTaskRunner runner = new AsyncTaskRunner();
+        String sleepTime = "5";
+        runner.execute(sleepTime);
+    }
+
     //    private void showToast() {
 //        //Todo remove textview and show toast with pagevisits instead
 //    }
+
+
+    //obtained from:  https://www.journaldev.com/9708/android-asynctask-example-tutorial
+
+    private class AsyncTaskRunner extends AsyncTask<String, String, String> {
+
+        private String resp;
+        ProgressDialog progressDialog;
+
+        @Override
+        protected String doInBackground(String... params) {
+            publishProgress("Dog is sleeping for..."); // Calls onProgressUpdate()
+            try {
+                int time = Integer.parseInt(params[0]) * 1000;
+                Thread.sleep(time);
+                resp = "Slept for " + params[0] + " seconds";
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                resp = e.getMessage();
+            } catch (Exception e) {
+                e.printStackTrace();
+                resp = e.getMessage();
+            }
+            return resp;
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            // execution of result of Long time consuming operation
+            progressDialog.dismiss();
+            dogslept.setText(result);
+        }
+
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = ProgressDialog.show(DogActivity.this,
+                    "ProgressDialog",
+                    "Dog is sleeping, wait a few seconds");
+        }
+
+
+        @Override
+        protected void onProgressUpdate(String... text) {
+            dogslept.setText(text[0]);
+        }
+    }
 }
